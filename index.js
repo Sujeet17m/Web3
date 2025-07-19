01b6460e8076375e7eef4523ef71136e1ea4c94f
Sujeet17m <sujeet17das@gmail.com>
2025-07-19 20:46:59 +0530

    uint8array

--- index.js
+++ index.js
@@ -28,26 +28,38 @@
 // console.log(`Input : ${result.input}`);
 // console.log(`Hash: ${result.hash}`);
 
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
-// Function to find an input string that produces a hash starting with the prefix
-function findHashWithPrefix(prefix) {
-    let input = 0;
-    while (true) {
-        let inputStr = "100xdevs" + input.toString();
-        // Generate SHA-256 hash and convert to hex string
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
-        if (hash.startsWith(prefix)) {
-            return { input: inputStr, hash: hash };
-        }
-        input++;
-    }
-}
+// // Function to find an input string that produces a hash starting with the prefix
+// function findHashWithPrefix(prefix) {
+//     let input = 0;
+//     while (true) {
+//         let inputStr = "100xdevs" + input.toString();
+//         // Generate SHA-256 hash and convert to hex string
+//         let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+//         if (hash.startsWith(prefix)) {
+//             return { input: inputStr, hash: hash };
+//         }
+//         input++;
+//     }
+// }
 
-// Find and print the input string and hash
-const result = findHashWithPrefix('0000');
-console.log(`Input: ${result.input}`);
-console.log(`Hash: ${result.hash}`);
+// // Find and print the input string and hash
+// const result = findHashWithPrefix('0000');
+// console.log(`Input: ${result.input}`);
+// console.log(`Hash: ${result.hash}`);
+
+const bytes = [202,256,1,23]
+//console.log(bytes);
+
+//A better way to represent an array of bytes is to use a UInt8Array in JS
+let byte = new Uint8Array([0,256,127,128]);
+//console.log(byte);
+
+let uint8Arr = new Uint8Array([1, 255, 127, 128]);
+uint8Arr[0] = 300;
+console.log(uint8Arr);
+
 
 
 
