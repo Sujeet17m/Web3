30ca9a6a2e8a78704f181ffd206ea04b78a59290
Sujeet17m <sujeet17das@gmail.com>
2025-09-11 06:35:25 +0530

    imported

--- sol_dapp/src/App.jsx
+++ sol_dapp/src/App.jsx
@@ -1,27 +1,32 @@
-
-import './App.css'
-import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import "./App.css";
+import React, { FC, useMemo } from "react";
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
-import '@solana/wallet-adapter-react-ui/styles.css';
+import "@solana/wallet-adapter-react-ui/styles.css";
 function App() {
-
   return (
     <>
-      <div>
-        Hi there!
-      </div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div>Hi there!</div>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
