d35909ebe1f79f910f68af6f621b12908d34b5ec
Sujeet17m <sujeet17das@gmail.com>
2025-09-21 10:21:40 +0530

    function sendairdroptouser

--- sol_dapp/src/Airdrop.jsx
+++ sol_dapp/src/Airdrop.jsx
@@ -1,6 +1,27 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Connection } from "@solana/web3.js";
+
+// the useWallet hook gives you access to the wallet object which has info about the connected wallet here useWallet 'hook' 'provides' the wallet variable inside the Airdrop function component
+
+//because of the useWallet hook we can access the wallet object and check if the wallet is connected or not and also get the public key of the connected wallet
+//because i wrapped the Airdrop component inside the WalletProvider in App.jsx file
 export function Airdrop() {
-    return <div>
-        <input type="text" placeholder="Amount"/>
-        <button>Send Airdrop</button>
+  //hooks in react
+  const wallet = useWallet();
+  // alert(wallet.connected);
+  // alert(wallet.publicKey.toString());
+
+  //define the function inside the component body
+  function sendAirdropToUser() {
+    alert("Airdrop sent to " + wallet.publicKey.toString());
+    Connection.requestAirdrop(wallet.publicKey, 10);
+  }
+
+  return (
+    <div>
+      {/* {wallet.connected ? wallet.publicKey.toString() : "Not connected"} */}
+      <input type="text" placeholder="Amount" />
+      <button onClick={sendAirdropToUser}>Send Airdrop</button>
     </div>
+  );
 }
